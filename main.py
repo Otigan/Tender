48cf9fac1ad7e157e8057e01444e3d2aae2dde0b
Otigan <braverobin98@gmail.com>
2020-04-19 17:35:08 +0700

    Testing p4a permissions

--- main.py
+++ main.py
@@ -18,7 +18,10 @@ from plyer import filechooser
 import ntpath
 import time
 import functools
-import jnius
+from jnius import autoclass
+from kivy.logger import Logger
+from pythonforandroid.recipes.android.src.android.permissions import request_permissions, Permission, check_permission
+import sys
 
 KV = '''
 #:import kivy kivy
@@ -183,62 +186,9 @@ Screen:
 '''
 
 
-
-def acquire_permissions(permissions, timeout=30):
-    """
-    blocking function for acquiring storage permission
-
-    :param permissions: list of permission strings , e.g. ["android.permission.READ_EXTERNAL_STORAGE",]
-    :param timeout: timeout in seconds
-    :return: True if all permissions are granted
-    """
-
-    PythonActivity = jnius.autoclass('org.kivy.android.PythonActivity')
-    Compat = jnius.autoclass('android.support.v4.content.ContextCompat')
-    currentActivity = jnius.cast('android.app.Activity', PythonActivity.mActivity)
-
-    checkperm = functools.partial(Compat.checkSelfPermission, currentActivity)
-
-    def allgranted(permissions):
-        """
-        helper function checks permissions
-        :param permissions: list of permission strings
-        :return: True if all permissions are granted otherwise False
-        """
-        return functools.reduce(lambda a, b: a and b,
-                                [True if p == 0 else False for p in map(checkperm, permissions)]
-                                )
-
-    haveperms = allgranted(permissions)
-    if haveperms:
-        # we have the permission and are ready
-        return True
-
-    # invoke the permissions dialog
-    currentActivity.requestPermissions(permissions, 0)
-
-    # now poll for the permission (UGLY but we cant use android Activity's onRequestPermissionsResult)
-    t0 = time.time()
-    while time.time() - t0 < timeout and not haveperms:
-        # in the poll loop we could add a short sleep for performance issues?
-        haveperms = allgranted(permissions)
-
-    return haveperms
-
 class ResultBox(BoxLayout):
     pass
 
-class MyScatterPlane(ScatterPlane):
-    def on_touch_up(self, touch):
-        if self.collide_point(*touch.pos):
-            if touch.is_mouse_scrolling:
-                if touch.button == 'scrolldown':
-                    mat = Matrix().scale(.9, .9, .9)
-                    self.apply_transform(mat, anchor=touch.pos)
-                elif touch.button == 'scrollup':
-                    mat = Matrix().scale(1.1, 1.1, 1.1)
-                    self.apply_transform(mat, anchor=touch.pos)
-        return super().on_touch_up(touch)
 
 
 class ContentNavigationDrawer(BoxLayout):
@@ -274,13 +224,21 @@ class TenderBot(MDApp):
     clients = ObjectProperty()
 
     def build(self):
-
         return Builder.load_string(KV)
 
     def file_manager_open(self):
-        filechooser.open_file(on_selection=self.handle_selection,
-                              title="Pick a CSV file",
-                              filters=[("*.csv")])
+
+        if(sys.platform == "linux"):
+            request_permissions([Permission.WRITE_EXTERNAL_STORAGE, ])
+            # For requesting permission you can pass a list with all the permissions you need
+            if (check_permission('android.permission.WRITE_EXTERNAL_STORAGE')):
+                filechooser.open_file(on_selection=self.handle_selection,
+                                      title="Pick a CSV file",
+                                      filters=[("*.csv")])
+        else:
+            filechooser.open_file(on_selection=self.handle_selection,
+                                  title="Pick a CSV file",
+                                  filters=[("*.csv")])
 
     def on_selection(self, *a, **k):
         '''
